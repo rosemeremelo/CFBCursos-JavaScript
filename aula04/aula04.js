e827d3fa0dbc05a430181769df9263db8b80c636
rosemeremelo <rosemeresvmelo@outlook.com>
2025-06-21 19:53:25 -0400

    atualização

--- aula04/aula04.js
+++ aula04/aula04.js
@@ -5,4 +5,5 @@ num2=10;
 
 res= num1 +  num2;
 
+
 console.log(res);
---
+++ aula05/aula05.js
@@ -0,0 +1,14 @@
+/*
+>
+<
+>=
+<=
+==!
+===
+*/
+
+let num1=10;
+let num2=5;
+let num3=20;
+console.log(num1 > num2); // true
+console.log(num1 < num2); // false
